39d7726ac69b7b554f1384cffacdc03b8628ad1b
radianid <54707882+radianid@users.noreply.github.com>
2019-09-13 16:21:53 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+window.onload = init();
+
+function init() {
+    setCanvas();
+    setMedia();
+
+    game = new Game();
+    game.run();
+}
+
+function setCanvas() {
+    canvas = document.getElementById('canvas');
+
+    cw = canvas.width = 960;
+    ch = canvas.height = 600;
+    pen = canvas.getContext('2d');
+}
+
+function setMedia(){
+    media = {};
+
+    media.plane = new Image();
+    media.plane.src = 'source/PNG/plane.png';
+    
+    media.bg = new Image();
+    media.bg.src = 'source/PNG/background.png';
+}
